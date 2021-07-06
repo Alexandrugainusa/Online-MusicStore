8958ac10c4d1bb565d924f215acedffb2f909cac
alexandrugainusa <alexandruu.gainusa@yahoo.com>
2021-07-06 15:55:20 +0300

    initialize project

---
+++ src/main/java/com/project/onlinemusicstore/Controller.java
@@ -0,0 +1,16 @@
+package com.project.onlinemusicstore;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/something")
+    public ResponseEntity<String> firstMessage(){
+        String s = "first message";
+        return new ResponseEntity<>(s, HttpStatus.OK);
+    }
+}
