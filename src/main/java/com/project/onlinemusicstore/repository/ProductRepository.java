3df50bc0a4436e7ca7afdeafbf5c6b10a4eb58f6
alexandrugainusa <alexandruu.gainusa@yahoo.com>
2022-05-11 20:55:26 +0300

    add service to project

--- src/main/java/com/project/onlinemusicstore/repository/ProductRepository.java
+++ src/main/java/com/project/onlinemusicstore/repository/ProductRepository.java
@@ -2,10 +2,17 @@ package com.project.onlinemusicstore.repository;
 
 import com.project.onlinemusicstore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
+    Product getProductByCategoryId(Integer id);
+
+    @Query(
+            value = "SELECT * FROM onlinemusicstore.product p where p.brand  LIKE  'g%' ",
+            nativeQuery = true)
+    Product findProductByGNative(String brand);
 
 }
